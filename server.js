1191be35a2496bcbe4fb77e8bd3993054325d383
Charl Roux <charlroux641@gmail.com>
2022-06-28 18:12:13 +0200

    Server configuration

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
 
-
-app.use(express.static('./dist/slider'));
-
-app.get('/*', function(req,res){
-    res.sendFile('index.html', {root: 'dist/slider'})
+app.use(express.static(__dirname + '/dist'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
